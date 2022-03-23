bb3796247f0450764a7ffad955b872f42a2230eb
buronpro <buronbek33@gmail.com>
2022-03-23 12:40:23 +0500

    tiwiter

---
+++ src/Localization/Content.js
@@ -0,0 +1,91 @@
+const content = {
+	uz: {
+		header: {
+			links: {
+				Home: 'Asosiy',
+				Explore: 'Tadqiq qiling',
+				Notifications: 'Bildirishnomalar',
+				Messages: 'Xabarlar',
+				Bookmarks: 'Xatcho‘plar',
+				Lists: `Ro'yxatlar`,
+				Profile: 'Profil',
+				More: `Ko'proq`,
+				logout: 'Chiqish',
+			},
+		},
+
+		main: {
+			Home: {
+				Home: 'Asosiy',
+				Placeholder: `Nimalar bo'lyapti`,
+			},
+
+			profile: {
+				Edit: 'Profilni tahrirlash',
+				Tweets: 'Tvitlar',
+				Replies: 'Tvitlar va javoblar',
+				Media: 'OAV',
+				Likes: 'Yoqtirganlari',
+				Following: 'Obunalar',
+				Follows: 'Obunachilar',
+			},
+		},
+
+		footer: {
+			Search: 'Twitterda qidiring',
+			Trends: 'Siz uchun trendlar',
+			Trending: 'Germaniyada trend',
+			Revolution: 'Inqilob',
+			Tweets: '50,4 ming Tweets',
+			More: `Ko'proq ko'rsatish`,
+			Like: 'Sizga yoqishi mumkin',
+			Follow: 'Kuzatmoq',
+		},
+	},
+
+	en: {
+		header: {
+			links: {
+				Home: 'Home',
+				Explore: 'Explore',
+				Notifications: 'Notifications',
+				Messages: 'Messages',
+				Bookmarks: 'Bookmarks',
+				Lists: `Lists`,
+				Profile: 'Profile',
+				More: `More`,
+				logout: 'Log out',
+			},
+		},
+
+		main: {
+			Home: {
+				Home: 'Home',
+				Placeholder: `What’s happening`,
+			},
+
+			profile: {
+				Edit: 'Edit profile',
+				Tweets: 'Tweets',
+				Replies: 'Tweets & replies',
+				Media: 'Media',
+				Likes: 'Likes',
+				Following: 'Following',
+				Follows: 'Follows',
+			},
+		},
+
+		footer: {
+			Search: 'Search Twitter',
+			Trends: 'Trends for you',
+			Trending: 'Trending in Germany',
+			Revolution: 'Revolution',
+			Tweets: '50.4K Tweets',
+			More: `Show more`,
+			Like: 'You might like',
+			Follow: 'Follow',
+		},
+	},
+};
+
+export default content;
